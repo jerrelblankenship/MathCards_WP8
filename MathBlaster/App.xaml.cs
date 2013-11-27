5fcb76f2bdfc20ee4f30d69f7b6a98945484596f
Jerrel Blankenship <jerrel@jerrelblankenship.com>
2013-11-27 03:41:19 -0500

    Started work on adding Rate My App functionality

--- MathBlaster/App.xaml.cs
+++ MathBlaster/App.xaml.cs
@@ -56,6 +56,7 @@ namespace MathBlaster
         // This code will not execute when the application is reactivated
         private void Application_Launching(object sender, LaunchingEventArgs e)
         {
+            MathBlaster.Helpers.FeedbackHelper.Default.Launching();
         }
 
         // Code to execute when the application is activated (brought to foreground)
